abcaf1cc8d841218bdd55b0dd6e9cc0a06047ea5
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2026-05-27 21:26:57 +0200

    Update run_local_proxy.sh

--- run_local_proxy.sh
+++ run_local_proxy.sh
@@ -6,8 +6,8 @@ PORT=8899 \
 BASE_API_URL=http://192.168.0.124:8888/v1 \
 BASE_API_KEY=1mmer \
 BASE_MODEL=gemma4 \
-MAX_TOKENS=3 \ # max bucket, 3 req
-REFILL_TOKENS=1 \ # after 300s gets 1 req
+MAX_TOKENS=3 \
+REFILL_TOKENS=1 \
 REFILL_INTERVAL_SECONDS=300 \
 ruby llm_proxy.rb
 
